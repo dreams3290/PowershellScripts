6bd7a8330e28cae46a6d6ef88b69c5848e9e3ba7
Sanjay <sramadugu1@gmail.com>
2018-03-08 11:14:08 -0600

    script to do IISreset remotely  is added

---
+++ RemoteIISReset.ps1
@@ -0,0 +1,10 @@
+﻿$servers="server1","server2"
+   foreach ($server in $servers)
+{
+    Write-Host "Restarting IIS on server $server..."
+    IISRESET $server /noforce
+    Write-Host "IIS status for server $server"
+    IISRESET $server /status
+}
+Write-Host IIS has been restarted on all servers
+

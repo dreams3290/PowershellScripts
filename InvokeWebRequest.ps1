351f7c80e50085ba73617d40821d5b5f91cda8a2
Sanjay Ramadugu <sramadugu1@gmail.com>
2019-07-11 09:55:32 -0500

    Invoking web request mainly for API's

---
+++ InvokeWebRequest.ps1
@@ -0,0 +1,6 @@
+﻿$dates=get-content "local data path"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+foreach ($date in $dates)
+{
+Invoke-RestMethod -Uri "enter URI here" -Method Post >>"path to the log file"
+}

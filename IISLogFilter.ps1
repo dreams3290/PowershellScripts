b9d4f35f3bcab948815a1caa6cc521ded1c28f2b
dreams3290 <sramadugu1@gmail.com>
2019-09-04 23:52:23 -0500

    more server management scripts

---
+++ IISLogFilter.ps1
@@ -0,0 +1,21 @@
+﻿
+function Convert-IISLogs {
+    [CmdletBinding()]
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateScript({ Test-Path -Path $_ })]
+        [string[]]
+        $path
+    )
+
+    Process {
+        forEach($filePath in $path) {
+            $headers = (Get-Content -Path $filePath -TotalCount 4 | Select -First 1 -Skip 3) -replace '#Fields: ' -split ' '
+            Get-Content $filePath | Select-String -Pattern '^#' -NotMatch | ConvertFrom-Csv -Delimiter ' ' -Header $headers
+        }
+    }
+}
+
+$path= "logs path"
+Get-ChildItem '$path\*.log' | Convert-IISLogs | Sort-Object cs-host -Unique | Select-Object  cs-host ,cs-uri-stem, sc-status | Format-List *  >IISlogResults12.txt
---
+++ compareServerPatches.ps1
@@ -0,0 +1,9 @@
+﻿$server01 = "server-01"
+
+$server02 = "server-02"
+
+$server01Patches = get-hotfix -computer $server01 | Where-Object {$_.HotFixID -ne “File 1”}
+
+$server02Patches = get-hotfix -computer $server02 | Where-Object {$_.HotFixID -ne “File 1”}
+
+Compare-Object ($server01Patches) ($server02Patches) -Property HotFixID
